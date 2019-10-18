bc9aeda21d33f872d691d48712bb2957385d2e5c
陈鹏飞 <tongzhi_fei@163.com>
2019-10-18 17:25:42 +0800

    fix client log

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -12,18 +12,28 @@ use GuzzleHttp\TransferStats;
 
 class Client extends \GuzzleHttp\Client
 {
+
+    protected $recordArgument = true;//日志记录参数
+    protected $recordResult = true;//日志记录结果
+
     public function __construct(array $config = [])
     {
         $config = array_merge(['timeout' => 10, 'verify' => false], $config);
+        if (isset($config['recordResult'])) {
+            $this->recordResult = $config['recordResult'];
+        }
+        if (isset($config['recordArgument'])) {
+            $this->recordArgument = $config['recordArgument'];
+        }
         parent::__construct($config);
     }
 
     public function request($method, $uri = '', array $options = [])
     {
         $log = [
-            'serviceStart' => (new \DateTime())->format('Y-m-d H:i:s.u')
+            'apiStart' => (new \DateTime())->format('Y-m-d H:i:s.u')
         ];
-        $options['headers']['X-Ca-Traceid'] = uuid('v4', true);
+        $options['headers']['X-Ca-Traceid'] = traceId();
         $options = array_merge(
             [
                 'on_stats' => function (TransferStats $stats) use (&$log, $uri) {
@@ -35,10 +45,12 @@ class Client extends \GuzzleHttp\Client
                 'force_ip_resolve' => 'v4'
             ]
             , $options);
-        $response = parent::request($method, $uri, $options);
 
         if (isset($options['query'])) {
             $log['arguments']['query'] = $options['query'];
+        }
+        if (isset($options['form_params'])) {
+            $log['arguments']['form_params'] = $options['form_params'];
         }
 
         if (isset($options['headers'])) {
@@ -52,14 +64,27 @@ class Client extends \GuzzleHttp\Client
         if (isset($options['json'])) {
             $log['arguments']['json'] = $options['json'];
         }
+        if (!$this->recordArgument) {//不记录参数
+            $log['arguments'] = [];
+        }
+        $log['arguments'] = json_encode($log['arguments'], JSON_UNESCAPED_UNICODE);
+        $log['method'] = $method;
 
-        $log['status'] = $response->getStatusCode();
-        $log['requestMethod'] = $method;
-        $log['serviceEnd'] = (new \DateTime())->format('Y-m-d H:i:s.u');
-        $log['result'] = $response->getBody()->getContents();
-        $response->getBody()->rewind();
-        info("http client", $log);
-//        Log::writeRunLog('api calls logging', $log);
-        return $response;
+        try {
+            $response = parent::request($method, $uri, $options);
+            $log['apiEnd'] = (new \DateTime())->format('Y-m-d H:i:s.u');
+            $log['status'] = $response->getStatusCode();
+            if ($this->recordResult) {
+                $log['result'] = $response->getBody()->getContents();
+            }
+            $response->getBody()->rewind();
+
+            info("http-client", ['apiRequest' => $log]);
+            return $response;
+        } catch (\Exception $e) {
+            $log['exception'] = $e->getMessage();
+            info("http-client-error", ['apiRequest' => $log]);
+            throw $e;
+        }
     }
 }
